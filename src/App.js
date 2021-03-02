79b13b928a4f97715ae270f5fb6dedb2b8fa0e78
levilu800b <levilu800b@gmail.com>
2021-03-02 20:55:41 +0000

    sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -17,6 +17,9 @@ function App() {
         >
           Learn React
         </a>
+
+        <br></br>
+        <button className="danger">test</button>
       </header>
     </div>
   );
